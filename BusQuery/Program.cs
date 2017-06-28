db2b58c51aff6cc7fe7ee41fa087880b30d5f2e1
andrecarlucci <andrecarlucci@gmail.com>
2017-06-28 10:04:58 -0300

    Add project files.

---
+++ BusQuery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
